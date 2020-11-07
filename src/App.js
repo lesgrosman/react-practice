c1f416ad5e2262568b920ac7a7a57a65029fd1c2
Glib <andreyagasov@gmail.com>
2020-11-07 16:27:37 +0100

    dynamic lists

--- src/App.js
+++ src/App.js
@@ -6,49 +6,72 @@ export  default class App extends Component {
   
   state = {
     cars: [
-      {name: 'Ford', year: 2018},
+      {name: 'Mercedes', year: 2018},
       {name: 'Audi', year: 2016},
-      {name: 'BMW', year: 1999},
+      {name: 'Mazda', year: 1999},
     ],
-    pageTitle: 'React Components'
+    pageTitle: 'React Components',
+    showCars: false
   }
 
-  changeTitleHandler = (newTitle) => {
+  toggleCarsHandler = () => {
     this.setState({
-      pageTitle: newTitle
+      showCars: !this.state.showCars
     })
   }
+
+  onChangeName(name, index) {
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+  }
+
+  deleteHandler(index) {
+    const cars = [...this.state.cars]
+    cars.splice(index, 1)
+    this.setState({cars})
+  }
+
 
   render() {
     const divStyle = {
       textAlign: 'center'
     }
 
-    const cars = this.state.cars;
+    let cars = null;
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return (
+          <Car 
+            key={index}
+            name={car.name} 
+            year={car.year}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+          />
+        )
+      })
+    }
 
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
-
+        
         <button 
-        onClick={this.changeTitleHandler.bind(this, 'Changed')}
-        >Change title</button>
+        onClick={this.toggleCarsHandler}
+        >Toggle cars</button>
 
-        <Car 
-          name={cars[0].name} 
-          year={cars[0].year}
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-          />
-        <Car 
-          name={cars[1].name} 
-          year={cars[1].year} 
-          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-          />
-        <Car
-          name={cars[2].name} 
-          year={cars[2].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
-          />
+        <div style={{
+          width: 400,
+          margin: 'auto',
+          padding: '20px'
+        }}>
+          { cars }
+        </div>
+     
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import './Car.css'
+import Radium from 'radium';
 
-export default (props) => (
-    <div>
-        <h3>Car name: {props.name}</h3>
-        <p>Year: <strong>{props.year}</strong></p>
-        <button onClick={props.onChangeTitle}>Click</button>
-    </div>
-) 
+const Car = props => {
+    const inputClasses = ['input']
+
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+
+    }
+
+    return(
+        <div className="Car" style={style}>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input 
+                type="text" 
+                onChange={props.onChangeName} 
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    ) 
+} 
+
+export default Radium(Car); 
 
 // export default class Car extends React.Component {
 //     render() {
